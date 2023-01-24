cd6ea14486c84ba6236ea5a137c91877007d9a45
RosenAndreevKolev <rosenandreevkolev@abv.bg>
2023-01-24 23:07:01 +0200

    Move project directory

--- BattleShipsOnline/src/bg/sofia/uni/fmi/mjt/battleships/common/ClientRequest.java
+++
@@ -1,8 +0,0 @@
-package bg.sofia.uni.fmi.mjt.battleships.common;
-
-public record ClientRequest(String input, String currentScreen, String username) {
-
-    public ClientRequest(String input, String currentScreen) {
-        this(input, currentScreen, null);
-    }
-}
--- BattleShipsOnline/src/bg/sofia/uni/fmi/mjt/battleships/server/command/Command.java
+++
@@ -1,4 +0,0 @@
-package bg.sofia.uni.fmi.mjt.battleships.server.command;
-
-public record Command(String command, String[] arguments) {
-}
